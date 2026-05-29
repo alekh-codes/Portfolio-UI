6aebee3132b0455cdc40662d22e6ebee7c9bbd78
alekh-thakur <alekhrathor8@gmail.com>
2026-05-29 22:42:09 +0530

    Updated resume

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RiDownload2Line } from "react-icons/ri";
 import main from "../assets/main.jpeg";
-import Resume from "../assets/ALEKH_MERN.pdf"
+import Resume from "../assets/Alekh_FrontendDeveloper_Resume.pdf"
 const Landing = ({dark}) => {
   return (
     <section id="home" className={`w-full ${dark ? "bg-black" : "bg-white"} `}>
