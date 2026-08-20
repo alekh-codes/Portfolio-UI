52a25842135c7ddc6aa2329a455bca6591058053
alekh <codewithalekh@gmail.com>
2026-08-20 21:43:30 +0530

    Fix: Secure credentials by using .env file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,77 +6,126 @@ import emailjs from "@emailjs/browser";
 const Contact = ({ dark }) => {
   const [emailError, setEmailerror] = useState("");
   const [formdata, setFormData] = useState({
-    name:"",
-    email:"",
-    message:""
-  }) 
-  const emailValidation = () =>{
-    if(!formdata.email.includes("@")){
+    name: "",
+    email: "",
+    message: "",
+  });
+  const emailValidation = () => {
+    if (!formdata.email.includes("@")) {
       setEmailerror("Invalid Email");
       return false;
     }
     setEmailerror("");
     return true;
-  }
+  };
 
-  const handleData = (e) =>{
+  const handleData = (e) => {
     setFormData({
-      ...formdata, [e.target.name] : e.target.value
-    })
-  }
+      ...formdata,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const isEmailValid = emailValidation();
     if (isEmailValid) {
-    emailjs
-      .send(
-        "service_ux0czhv",
-        "template_7kzkimk",
-        formdata,
-        "vy9JL-09yyzrCWmKK"
-      )
-      .then(() => {
-        alert("Message sent successfully");
+      emailjs
+        .send(
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
+          formdata,
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+        )
+        .then(() => {
+          alert("Message sent successfully");
 
-        setFormData({
-          name: "",
-          email: "",
-          message: ""
+          setFormData({
+            name: "",
+            email: "",
+            message: "",
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Failed to send message");
         });
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("Failed to send message");
-      });
-  }
-  }
+    }
+  };
   return (
     <section id="contact" className="flex flex-col pb-20 w-full">
-      <div data-aos = "fade-up" data-aos-duration = "1000" className="flex justify-center">
+      <div
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="flex justify-center"
+      >
         <h1 className="text-4xl font-bold mt-10">
           Get in <span className="text-purple-700">Touch</span>
         </h1>
       </div>
-      <div className="mt-7 mx-7 flex justify-center" data-aos = "fade-up" data-aos-duration = "1000">
+      <div
+        className="mt-7 mx-7 flex justify-center"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
         <p className="opacity-60">
           Have a question or want to work together? Drop me a message!
         </p>
       </div>
       <div className="flex justify-center">
-        <div className={`mt-4 w-full md:w-120 ${dark ? "bg-[#4f4b4b9b]":"bg-[#dee8f7]"} p-7 mx-7  rounded-xl`} data-aos = "fade-up" data-aos-duration = "1000">
-        <form action="" className="flex flex-col " onSubmit={handleSubmit}>
-            
-                <label className="mb-2" htmlFor="name" >Name</label>
-                <input type="text" id="name" name="name" value={formdata.name} className={`${dark?"bg-[#000000ce]":"bg-white"} rounded-xl px-3 py-2 mb-4`} placeholder="Your name" onChange={handleData}/>
-                <label className="mb-2" htmlFor="email">E-mail</label>
-                <input type="email" value={formdata.email} className={`${dark?"bg-[#000000ce]":"bg-white"} rounded-xl px-3 py-2 `} name="email" id="email" placeholder="Your Email" onChange={handleData}/>
-                <span className="text-[12px] ml-1 mb-3  text-red-500">{emailError}</span>
-                <label className="mb-2" htmlFor="message">Message</label>
-                <textarea name="message" value={formdata.message}  className={`${dark?"bg-[#000000ce]":"bg-white"} rounded-xl px-3 py-2 mb-4`} id="message" placeholder="Your message..." onChange={handleData}></textarea>
-                <button type="submit" className=" cursor-pointer flex items-center justify-center bg-purple-700 hover:bg-purple-800 px-3 py-2 rounded-xl text-white  ">< RiSendPlaneFill className="mr-2"  />Send Message</button>
-        </form>
-      </div>
+        <div
+          className={`mt-4 w-full md:w-120 ${dark ? "bg-[#4f4b4b9b]" : "bg-[#dee8f7]"} p-7 mx-7  rounded-xl`}
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <form action="" className="flex flex-col " onSubmit={handleSubmit}>
+            <label className="mb-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formdata.name}
+              className={`${dark ? "bg-[#000000ce]" : "bg-white"} rounded-xl px-3 py-2 mb-4`}
+              placeholder="Your name"
+              onChange={handleData}
+            />
+            <label className="mb-2" htmlFor="email">
+              E-mail
+            </label>
+            <input
+              type="email"
+              value={formdata.email}
+              className={`${dark ? "bg-[#000000ce]" : "bg-white"} rounded-xl px-3 py-2 `}
+              name="email"
+              id="email"
+              placeholder="Your Email"
+              onChange={handleData}
+            />
+            <span className="text-[12px] ml-1 mb-3  text-red-500">
+              {emailError}
+            </span>
+            <label className="mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              name="message"
+              value={formdata.message}
+              className={`${dark ? "bg-[#000000ce]" : "bg-white"} rounded-xl px-3 py-2 mb-4`}
+              id="message"
+              placeholder="Your message..."
+              onChange={handleData}
+            ></textarea>
+            <button
+              type="submit"
+              className=" cursor-pointer flex items-center justify-center bg-purple-700 hover:bg-purple-800 px-3 py-2 rounded-xl text-white  "
+            >
+              <RiSendPlaneFill className="mr-2" />
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
